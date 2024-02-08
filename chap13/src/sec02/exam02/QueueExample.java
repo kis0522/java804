a98c910156794666c91593528a23c69b497be673
kis0522 <kiminsu0522@naver.com>
2024-02-08 15:27:52 +0900

    chap13.sec02.exam02

---
+++ chap13/src/sec02/exam02/QueueExample.java
@@ -0,0 +1,31 @@
+package sec02.exam02;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+		Queue<Message> messageQueue = new LinkedList<Message>();
+		
+		messageQueue.offer(new Message("sendMail", "홍길동"));		//offer = put 대신 사용
+		messageQueue.offer(new Message("sendSMS", "오윤석"));
+		messageQueue.offer(new Message("sendKakaotalk", "홍두께"));
+		
+		while(!messageQueue.isEmpty()) {				//messageQueue가 비워지지 않았을때 실행
+			Message message = messageQueue.poll();		//message 에 messageQueue을 가져온다.
+			switch(message.command) {
+			case "sendMail":
+				System.out.println(message.to + "님에게 메일을 보냅니다.");
+				break;
+			case "sendSMS":
+				System.out.println(message.to + "님에게 SMS를 보냅니다.");
+				break;
+			case "sendKakaotalk":
+				System.out.println(message.to + "님에게 카카오톡을 보냅니다.");
+				break;
+			}
+		}
+	}
+
+}
